f49ceabf5f1f6fbefae97492b45edd22e929cc16
Daniel M. Martin <danielmmartin@gmail.com>
2021-08-11 14:08:27 -0300

    suggestion for a dry-run involving tags

--- tag_and_bump.sh
+++ tag_and_bump.sh
@@ -1,16 +1,19 @@
-# TODO: This will not be a dry run in any circumstance...
-git tag -m "make auto tag" -a "v$(poetry version -s)" && poetry version patch
-# TODO: we need to push this tag, right? Should it be in this script?
-# I think it should not, just in the veryu last step (definitely not for dry run)
-git push origin --tags
-
-# TODO: Option for doing a dry run: set env var DRYRUN before calling make, like
+# TODO: add option for doing a dry run: set env var DRYRUN before calling make, like
 # prompt> DRYRUN=True make
 # Cons: do not now if $(MAKE) -C ... inherits env vars (I would assume so, but needs checking)
 if [ "$DRYRUN" != "" ]
 then
-  # simulate tagging
+  # simulate creating tag here (THIS IS TRICKY)
+  # We could do, like, a lightweight tag and remove it in the
+  # last step (after poetry publish --dry-run)
+  # git tag "v$(poetry version -s)"
+  # Later (some other script? makefile?) we can cleanup *all* lightweight tags with
+  # git for-each-ref refs/tags/ --format '%(objecttype) %(refname:short)' |
+  #    while read ty name; do [ $ty = commit ] && git tag -d $name; done
 else
-  # do the real tagging
+  # TODO: what info put in tag message, I used just a simple msg
+  git tag -m "make auto tag" -a "v$(poetry version -s)" && poetry version patch
+  # TODO: Do we push it now to git? I would think so...
+  git push origin --tags
 fi
 
